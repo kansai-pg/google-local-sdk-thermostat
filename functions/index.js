a4a2e0c033749e4731b42f29176c862f08b4f86b
yep i'm student <takumi@mail.japan-is.fun>
2023-11-22 07:51:38 +0000

    set use sub

--- functions/index.js
+++ functions/index.js
@@ -57,13 +57,13 @@ const app = smarthome();
 app.onSync(async (body, headers) => {
   const userinfo = await getauth0_userinfo(headers)
   functions.logger.log("auth0_req", userinfo);
-  functions.logger.log("set_email", userinfo.email);
+  functions.logger.log("set_sub", userinfo.sub);
   return {
     requestId: body.requestId,
     payload: {
       agentUserId: USER_ID,
       devices: [{
-        id: userinfo.email,
+        id: userinfo.sub,
         type: 'action.devices.types.THERMOSTAT',
         traits: [
           'action.devices.traits.TemperatureSetting',
