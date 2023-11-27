53ab1d887b85ad53c03647c61acb901c2c8708af
yep i'm student <takumi@mail.japan-is.fun>
2023-11-27 09:46:09 +0900

    upload full m5stack code

--- local-home-sdk-esp32/firebase.h
+++ local-home-sdk-esp32/firebase.h
@@ -1,6 +1,6 @@
 #include <Adafruit_SHT31.h>
 #include <Firebase_ESP_Client.h>
-#define DATABASE_URL ""
+#define DATABASE_URL "testpro-21356-default-rtdb.firebaseio.com"
 #define WIZNET_RESET_PIN -1
 #define WIZNET_CS_PIN 19
 #define WIZNET_MISO_PIN 23
@@ -14,7 +14,6 @@ public:
   // static void task(void *pvParameters);
 };
 
-
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
 EthernetClient client;
@@ -25,13 +24,15 @@ FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
 
+// 温度・湿度センサーの初期化失敗するとLEDが赤色に数回光りマイコンが再起動
 void FirebaseDataClass::begin() {
   if (!sht31.begin(0x45)) {  // 0x45 is the I2C address of SHT31 sensor
     Serial.println("Could not find a valid SHT31 sensor, check wiring!");
     blinkLED_class.blinkLED(CRGB::Red, 1000);  // Continuous red LED blinking
     ESP.restart();
   }
-
+  // 認証なしでアクセスする（テストモード）
+  // 少なくとも1分後には値が更新されるので変な値を入れられてもたぶん大丈夫
   config.signer.test_mode = true;
 
   // Real time data baseのURLを割り当てる
@@ -48,12 +49,12 @@ int count = 0;
 void FirebaseDataClass::task() {
   float temperature = sht31.readTemperature();
   float humidity = sht31.readHumidity();
-
+  // 1分待機
   if (millis() - dataMillis > 60000){
     dataMillis = millis();
     // thermostatはユーザーIDへ置き換える(functions/index.jsのuserinfo.subの中身の文字列)
-    Serial.printf("Set int... %s\n", Firebase.RTDB.setInt(&fbdo, "/users/Ambient/thermostat/thermostatTemperatureAmbient", temperature) ? "ok" : fbdo.errorReason().c_str());
-    Serial.printf("Set int... %s\n", Firebase.RTDB.setInt(&fbdo, "/users/Ambient/thermostat/thermostatHumidityAmbient", humidity) ? "ok" : fbdo.errorReason().c_str());
+    Serial.printf("Set int... %s\n", Firebase.RTDB.setInt(&fbdo, "/users/Ambient/google-oauth2|101581906579469553343/thermostatTemperatureAmbient", temperature) ? "ok" : fbdo.errorReason().c_str());
+    Serial.printf("Set int... %s\n", Firebase.RTDB.setInt(&fbdo, "/users/Ambient/google-oauth2|101581906579469553343/thermostatHumidityAmbient", humidity) ? "ok" : fbdo.errorReason().c_str());
 
     }
 }
