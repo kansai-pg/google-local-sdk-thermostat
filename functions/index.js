156d01a3926c9e83348ba5071f6679d2f1588520
yep i'm student <takumi@mail.japan-is.fun>
2023-12-27 16:10:40 +0900

    remove non use func

--- functions/index.js
+++ functions/index.js
@@ -267,22 +267,3 @@ app.onDisconnect((body, headers) => {
 });
 
 exports.smarthome = functions.https.onRequest(app);
-
-// 更新時に呼び出されていると思う関数
-// （よくわかってない）
-exports.requestsync = functions.https.onRequest(async (request, response) => {
-  response.set('Access-Control-Allow-Origin', '*');
-  functions.logger.info(`Request SYNC for user ${USER_ID}`);
-  try {
-    const res = await homegraph.devices.requestSync({
-      requestBody: {
-        agentUserId: USER_ID,
-      },
-    });
-    functions.logger.info('Request sync response:', res.status, res.data);
-    response.json(res.data);
-  } catch (err) {
-    functions.logger.error(err);
-    response.status(500).send(`Error requesting sync: ${err}`);
-  }
-});
